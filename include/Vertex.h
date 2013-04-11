a31dd6e153a0113f8fe0958930164790fb8b3e94
John J. Glynn IV <jjg1914@rit.edu>
2013-04-10 23:49:30 -0400

    Added casts for Vertex to Matrix.

--- include/Vertex.h
+++ include/Vertex.h
@@ -1,11 +1,17 @@
 #ifndef H_VERTEX
 #define H_VERTEX
 
+#include "Matrix.h"
+
 struct Vertex {
 
   Vertex( double x, double y );
 
+  explicit Vertex( const Matrix<3,1>& src );
+
   bool operator<( const Vertex& b );
+
+  operator Matrix<3,1>();
 
   double x;
 
--- src/Vertex.cpp
+++ src/Vertex.cpp
@@ -1,3 +1,5 @@
+#include <cassert>
+
 #include "Vertex.h"
 
 using namespace std;
@@ -8,7 +10,21 @@ Vertex::Vertex( double x, double y )
 {
 }
 
+Vertex::Vertex( const Matrix<3,1>& src )
+  : x( src[0][0] ),
+    y( src[1][0] )
+{
+}
+
 bool Vertex::operator<( const Vertex& b )
 {
   return ( y < b.y ) || ( y == b.y && x < b.x );
+}
+
+Vertex::operator Matrix<3,1>()
+{
+  Matrix<3,1> result;
+  result[0][0] = x;
+  result[1][0] = y;
+  result[2][0] = 1;
 }
