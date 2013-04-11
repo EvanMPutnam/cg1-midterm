64235fbc762f94890cd3f1b56e1d65a8c90ad330
John J. Glynn IV <jjg1914@rit.edu>
2013-04-11 00:10:35 -0400

    Added apply method to polygon.

--- include/Polygon.h
+++ include/Polygon.h
@@ -21,10 +21,20 @@ public:
 
   std::vector<Edge> edges() const;
 
+  template<typename T>
+  void apply( const T& t );
+
 private:
 
   std::vector<Vertex> vertices;
 
 };
+
+template<typename T>
+void Polygon::apply( const T& t ) {
+  for ( int i = 0; i < vertices.size(); ++i ) {
+    t( vertices[i] );
+  }
+}
 
 #endif
